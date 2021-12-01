5e17de028c6054210c3db4365c92e6f468acd07c
Tatiane Entler <94848789+tatientler@users.noreply.github.com>
2021-12-01 15:35:46 -0300

    Novos exercícios de JS

---
+++ aula13/ambiente.js
@@ -0,0 +1,27 @@
+var c = 1  // Lógica no final
+do {
+    console.log(`Passo ${c}`)
+    c++
+} while (c <= 6)
+
+
+/* - Lógica no início
+var c = 1
+while (c <= 6) {
+    console.log(`Passo ${c}`)
+    c++  //c = c + 1
+}
+*/
+
+
+/* - Forma braçal
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+*/
